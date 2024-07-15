979b6ca62de3de3c43bea81f96a66bde48499a16
mdasifahamed <mdasifahamed89@yahoo.com>
2024-07-15 15:35:33 +0600

    Storage Feature
    Now Current Tab Url Can Stored To The Local Storage

--- popup.js
+++ popup.js
@@ -9,21 +9,27 @@ let url,urlStorage;
 
 document.addEventListener('DOMContentLoaded', async(e)=>{
     e.preventDefault();
-    url = 'www.faceboo.com';
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions)
+    url = tab.url;
     urlPlaceHolder.innerText= url
-    urlStorage = await chrome.storage.local.get(['urlStorage'])
-    urlStorage = urlStorage.urlStorage
+    
 })
 blockBtn.addEventListener('click',async (e)=>{
 
     e.preventDefault();
+    urlStorage = await chrome.storage.local.get(['urlStorage'])
+    urlStorage = urlStorage.urlStorage
     urlStorage.push(url)
     await chrome.storage.local.set({urlStorage:urlStorage})
+    alert(url)
 
 })
 
-listBtn.addEventListener('click',(e)=>{
+listBtn.addEventListener('click',async(e)=>{
     e.preventDefault();
+    urlStorage = await chrome.storage.local.get(['urlStorage'])
+    urlStorage = urlStorage.urlStorage
     console.log(urlStorage)
 
 })
